84121c16f6844e82b542844ea0c40d206ca7e11f
Daan Haitsma <daan@haitsma-dejager.nl>
2019-03-03 14:32:35 +0100

    Minor textual corrections in README and comments (#2)
    * Minor textual corrections in README and comments
    
    * Revert accidental change of composer command

--- config/config.php
+++ config/config.php
@@ -7,8 +7,8 @@ return [
     | Default Resource Abilities
     |--------------------------------------------------------------------------
     |
-    | This array of abilities will be used to determine which cruddy tests
-    | needs to be made. Feel free to change this into something you want.
+    | This array of abilities will be used to determine which cruddy tests need
+    | to be created. Feel free to change this into something you want.
     |
     */
 
